d1d0867cd5b85204110a51fae9d2a94d8ab30aa8
= <lasazawa@gmail.com>
2014-10-15 20:11:16 -0700

    first round of changes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var request = require("request");
+bodyParser = require("body-parser");
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var favorites = [];
+var count = 1;
 
 app.get('/search', function(req, res){
 
@@ -20,4 +26,34 @@ app.get('/search', function(req, res){
   });
 });
 
-app.listen(3000);
+app.get('/movie/:id', function(req, res) {
+    //use movie id to find more info about this film
+    var filmId = req.params.id;
+    var url = "http://www.omdbapi.com/?i=" + filmId;
+
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var film = JSON.parse(body);
+            res.render("film.ejs", {filmDetails: film});
+        }
+    });
+
+
+});
+app.get('/favorites', function(req, res) {
+    res.render("favorites.ejs", {favoritesList: favorites});
+});
+
+app.post('/favorites', function(req, res) {
+    var favoritedFilm = {};
+    favoritedFilm.title = req.body.movie.title;
+    favoritedFilm.count = count;
+    count++;
+    favorites.push(favoritedFilm);
+    console.log(favorites);
+    res.render("favorites.ejs", {favoritesList: favorites});
+});
+
+
+
+app.listen(3333);
